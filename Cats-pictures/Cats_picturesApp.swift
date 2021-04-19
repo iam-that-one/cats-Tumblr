58b6924a70f04af73de0b35fe4387e1f11fd5859
iam-that-one <iam-that-one@users.noreply.github.com>
2021-04-20 01:04:18 +0300

    Initial Commit

---
+++ Cats-pictures/Cats_picturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cats_picturesApp.swift
+//  Cats-pictures
+//
+//  Created by Abdullah Alnutayfi on 20/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct Cats_picturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cats-pictures/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cats-pictures
+//
+//  Created by Abdullah Alnutayfi on 20/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
