9e46b24e41bba0e5a40230e00e1cbf29fb602f21
iam-that-one <iam-that-one@users.noreply.github.com>
2021-04-22 22:29:22 +0300

    start

---
+++ Cats-pictures/CatsViewModel.swift
@@ -0,0 +1,323 @@
+//
+//  CatsViewModel.swift
+//  Cats-pictures
+//
+//  Created by Abdullah Alnutayfi on 20/04/2021.
+//
+
+import Foundation
+import SwiftUI
+class CatsViewModel: ObservableObject{
+    @Published var cats = [Cat]()
+    func getCat(){
+        
+        let headers = ["x-api-key": "DEMO-API-KEY"]
+        
+        let request = NSMutableURLRequest(url: NSURL(string: "https://api.thecatapi.com/v1/images/search")! as URL,
+                                          cachePolicy: .useProtocolCachePolicy,
+                                          timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print(error!)
+            } else {
+                if let data = data{
+                    do{
+                        let result = try JSONDecoder().decode([Cat].self, from: data)
+                        print(result)
+                        DispatchQueue.main.async {
+                            self.cats.append(contentsOf: result)
+                        }
+                    }catch{
+                        print(error)
+                    }
+                }
+              //  let httpResponse = response as? HTTPURLResponse
+              //  print(httpResponse)
+            }
+        })
+        
+        dataTask.resume()
+    }
+    func loadImage(ImageUrl: String) -> Data?{
+        guard let url = URL(string: ImageUrl) else {return Data()}
+        if let data = try? Data(contentsOf: url){
+            return data
+        }
+        return nil
+    }
+}
+
+struct Cat: Codable, Identifiable {
+    let breeds, categories: [JSONAny]?
+    let id: String?
+    let url: String?
+    /*
+    let width: String
+    let height: String
+    
+    var cgWith: CGFloat{
+        if let n = NumberFormatter().number(from: width) {
+            let f = CGFloat(truncating: n)
+            return f
+    }
+        return 0
+    }
+      
+        var cgHeight: CGFloat{
+            if let n = NumberFormatter().number(from: height) {
+                let f = CGFloat(truncating: n)
+                return f
+        }
+            return 0
+        }
+        
+        
+*/
+
+
+
+
+class JSONNull: Codable {
+    
+    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
+        return true
+    }
+    
+   // public var hashValue: Int {
+     //   return 0
+   // }
+    
+    public init() {}
+    
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if !container.decodeNil() {
+            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
+        }
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encodeNil()
+    }
+}
+
+class JSONCodingKey: CodingKey {
+    let key: String
+    
+    required init?(intValue: Int) {
+        return nil
+    }
+    
+    required init?(stringValue: String) {
+        key = stringValue
+    }
+    
+    var intValue: Int? {
+        return nil
+    }
+    
+    var stringValue: String {
+        return key
+    }
+}
+
+class JSONAny: Codable {
+    
+    let value: Any
+    
+    static func decodingError(forCodingPath codingPath: [CodingKey]) -> DecodingError {
+        let context = DecodingError.Context(codingPath: codingPath, debugDescription: "Cannot decode JSONAny")
+        return DecodingError.typeMismatch(JSONAny.self, context)
+    }
+    
+    static func encodingError(forValue value: Any, codingPath: [CodingKey]) -> EncodingError {
+        let context = EncodingError.Context(codingPath: codingPath, debugDescription: "Cannot encode JSONAny")
+        return EncodingError.invalidValue(value, context)
+    }
+    
+    static func decode(from container: SingleValueDecodingContainer) throws -> Any {
+        if let value = try? container.decode(Bool.self) {
+            return value
+        }
+        if let value = try? container.decode(Int64.self) {
+            return value
+        }
+        if let value = try? container.decode(Double.self) {
+            return value
+        }
+        if let value = try? container.decode(String.self) {
+            return value
+        }
+        if container.decodeNil() {
+            return JSONNull()
+        }
+        throw decodingError(forCodingPath: container.codingPath)
+    }
+    
+    static func decode(from container: inout UnkeyedDecodingContainer) throws -> Any {
+        if let value = try? container.decode(Bool.self) {
+            return value
+        }
+        if let value = try? container.decode(Int64.self) {
+            return value
+        }
+        if let value = try? container.decode(Double.self) {
+            return value
+        }
+        if let value = try? container.decode(String.self) {
+            return value
+        }
+        if let value = try? container.decodeNil() {
+            if value {
+                return JSONNull()
+            }
+        }
+        if var container = try? container.nestedUnkeyedContainer() {
+            return try decodeArray(from: &container)
+        }
+        if var container = try? container.nestedContainer(keyedBy: JSONCodingKey.self) {
+            return try decodeDictionary(from: &container)
+        }
+        throw decodingError(forCodingPath: container.codingPath)
+    }
+    
+    static func decode(from container: inout KeyedDecodingContainer<JSONCodingKey>, forKey key: JSONCodingKey) throws -> Any {
+        if let value = try? container.decode(Bool.self, forKey: key) {
+            return value
+        }
+        if let value = try? container.decode(Int64.self, forKey: key) {
+            return value
+        }
+        if let value = try? container.decode(Double.self, forKey: key) {
+            return value
+        }
+        if let value = try? container.decode(String.self, forKey: key) {
+            return value
+        }
+        if let value = try? container.decodeNil(forKey: key) {
+            if value {
+                return JSONNull()
+            }
+        }
+        if var container = try? container.nestedUnkeyedContainer(forKey: key) {
+            return try decodeArray(from: &container)
+        }
+        if var container = try? container.nestedContainer(keyedBy: JSONCodingKey.self, forKey: key) {
+            return try decodeDictionary(from: &container)
+        }
+        throw decodingError(forCodingPath: container.codingPath)
+    }
+    
+    static func decodeArray(from container: inout UnkeyedDecodingContainer) throws -> [Any] {
+        var arr: [Any] = []
+        while !container.isAtEnd {
+            let value = try decode(from: &container)
+            arr.append(value)
+        }
+        return arr
+    }
+    
+    static func decodeDictionary(from container: inout KeyedDecodingContainer<JSONCodingKey>) throws -> [String: Any] {
+        var dict = [String: Any]()
+        for key in container.allKeys {
+            let value = try decode(from: &container, forKey: key)
+            dict[key.stringValue] = value
+        }
+        return dict
+    }
+    
+    static func encode(to container: inout UnkeyedEncodingContainer, array: [Any]) throws {
+        for value in array {
+            if let value = value as? Bool {
+                try container.encode(value)
+            } else if let value = value as? Int64 {
+                try container.encode(value)
+            } else if let value = value as? Double {
+                try container.encode(value)
+            } else if let value = value as? String {
+                try container.encode(value)
+            } else if value is JSONNull {
+                try container.encodeNil()
+            } else if let value = value as? [Any] {
+                var container = container.nestedUnkeyedContainer()
+                try encode(to: &container, array: value)
+            } else if let value = value as? [String: Any] {
+                var container = container.nestedContainer(keyedBy: JSONCodingKey.self)
+                try encode(to: &container, dictionary: value)
+            } else {
+                throw encodingError(forValue: value, codingPath: container.codingPath)
+            }
+        }
+    }
+    
+    static func encode(to container: inout KeyedEncodingContainer<JSONCodingKey>, dictionary: [String: Any]) throws {
+        for (key, value) in dictionary {
+            let key = JSONCodingKey(stringValue: key)!
+            if let value = value as? Bool {
+                try container.encode(value, forKey: key)
+            } else if let value = value as? Int64 {
+                try container.encode(value, forKey: key)
+            } else if let value = value as? Double {
+                try container.encode(value, forKey: key)
+            } else if let value = value as? String {
+                try container.encode(value, forKey: key)
+            } else if value is JSONNull {
+                try container.encodeNil(forKey: key)
+            } else if let value = value as? [Any] {
+                var container = container.nestedUnkeyedContainer(forKey: key)
+                try encode(to: &container, array: value)
+            } else if let value = value as? [String: Any] {
+                var container = container.nestedContainer(keyedBy: JSONCodingKey.self, forKey: key)
+                try encode(to: &container, dictionary: value)
+            } else {
+                throw encodingError(forValue: value, codingPath: container.codingPath)
+            }
+        }
+    }
+    
+    static func encode(to container: inout SingleValueEncodingContainer, value: Any) throws {
+        if let value = value as? Bool {
+            try container.encode(value)
+        } else if let value = value as? Int64 {
+            try container.encode(value)
+        } else if let value = value as? Double {
+            try container.encode(value)
+        } else if let value = value as? String {
+            try container.encode(value)
+        } else if value is JSONNull {
+            try container.encodeNil()
+        } else {
+            throw encodingError(forValue: value, codingPath: container.codingPath)
+        }
+    }
+    
+    public required init(from decoder: Decoder) throws {
+        if var arrayContainer = try? decoder.unkeyedContainer() {
+            self.value = try JSONAny.decodeArray(from: &arrayContainer)
+        } else if var container = try? decoder.container(keyedBy: JSONCodingKey.self) {
+            self.value = try JSONAny.decodeDictionary(from: &container)
+        } else {
+            let container = try decoder.singleValueContainer()
+            self.value = try JSONAny.decode(from: container)
+        }
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        if let arr = self.value as? [Any] {
+            var container = encoder.unkeyedContainer()
+            try JSONAny.encode(to: &container, array: arr)
+        } else if let dict = self.value as? [String: Any] {
+            var container = encoder.container(keyedBy: JSONCodingKey.self)
+            try JSONAny.encode(to: &container, dictionary: dict)
+        } else {
+            var container = encoder.singleValueContainer()
+            try JSONAny.encode(to: &container, value: self.value)
+        }
+    }
+}
+}
--- Cats-pictures/ContentView.swift
+++ Cats-pictures/ContentView.swift
@@ -8,9 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var stillLoding = true
+    @StateObject var vm = CatsViewModel()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(){
+            Color.black.opacity(0.70)
+            if stillLoding{
+              ProgressView()
+                .progressViewStyle(CircularProgressViewStyle(tint: .white))
+                .scaleEffect(1)
+                
+            }
+           
+        ScrollView{
+            Text("All images fetched from Tumblr")
+                .font(.system(size: 15, weight: .bold, design: .rounded))
+                .frame(width: 3000, height: 50)
+                .foregroundColor(Color.black)
+                .shadow(color:.white,radius: 10)
+            VStack{
+            Spacer()
+       
+            //Spacer()
+            Button{
+                stillLoding = true
+                withAnimation(){
+                vm.cats = []
+                }
+                vm.getCat()
+                
+            }label:{
+               Text("Show new Kitty")
+                .font(.system(size: 15, weight: .bold, design: .rounded))
+                    .frame(width: 200, height: 35)
+                .background(Color.green)
+                .cornerRadius(10)
+                .foregroundColor(Color.black)
+                    .shadow(color:.white,radius: 10)
+                .padding(.bottom,20)
+            }
+        
+        
+           
+           // Spacer()
+                
+            ForEach(vm.cats){cat in
+           
+                Image(uiImage: UIImage(data: vm.loadImage(ImageUrl: cat.url ?? "") ?? Data()) ??  UIImage())
+                    .resizable()
+                    .scaledToFit().cornerRadius(10)
+                    .frame(width: 400, height: 400)
+                    
+                    .shadow(radius: 10)
+                
+                // Spacer()
+            }.onAppear{stillLoding = false}
+                
+        }.onAppear(){vm.getCat()}
+          
+        }.offset(y: 200)
+            
+          
+            
+        }.ignoresSafeArea()
     }
 }
 
